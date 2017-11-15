6095d48bc19146f58822387ac1f840196f6871dc
tlindener <tobias.lindener@outlook.com>
2017-11-15 14:43:51 +0100

    Add topic creation

--- start.sh
+++ start.sh
@@ -4,6 +4,9 @@ do
 raw = "raw-video-stream-" + $number
 person = "person-video-stream-" + $number
 face = "face-video-stream-" + $number
+~/kafka/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic $face
+~/kafka/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic $person
+~/kafka/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic $raw
 # docker run --network samaritan_kafkanet --name tlindener/samaritan-face-detection
 # docker run --network samaritan_kafkanet --name tlindener/samaritan-person-detection
 # docker run --network samaritan_kafkanet --name tlindener/samaritan-image-extraction
