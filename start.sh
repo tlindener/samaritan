7563bb59ce3fed3841804d46c5bf7f6daf74b0af
tlindener <tobias.lindener@gmail.com>
2017-12-04 17:51:24 +0100

    enable gpu

--- start.sh
+++ start.sh
@@ -8,9 +8,9 @@ base="video-stream-"$number
 ~/kafka/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic $face
 ~/kafka/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic $person
 ~/kafka/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic $raw
-docker run -itd --network samaritan_kafkanet --name $face tlindener/samaritan-face-detection python -u consumer.py --topic $base
-docker run -itd --network samaritan_kafkanet --name  $person tlindener/samaritan-person-detection  python -u consumer.py --topic $base
-docker run -itd --network samaritan_kafkanet --name  $raw tlindener/samaritan-frame-extractor  python -u producer.py --topic $raw
+docker run --runtime=nvidia -itd --network samaritan_kafkanet --name $face tlindener/samaritan-face-detection python -u consumer.py --topic $base
+docker run --runtime=nvidia -itd --network samaritan_kafkanet --name  $person tlindener/samaritan-person-detection  python -u consumer.py --topic $base
+docker run --runtime=nvidia -itd --network samaritan_kafkanet --name  $raw tlindener/samaritan-frame-extractor  python -u producer.py --topic $raw
 
 echo "$number"
 done
